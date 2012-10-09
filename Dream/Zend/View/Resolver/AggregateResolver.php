e056a7dfa71067b1ca47d35f189534688d597ff6
Dreamd <dreamd@Andys-iMac-Office.local>
2012-10-09 13:08:25 +0800

    fix no template , change the name to ucwords and than load parent actions

--- Dream/Zend/View/Resolver/AggregateResolver.php
+++ Dream/Zend/View/Resolver/AggregateResolver.php
@@ -10,27 +10,6 @@ use Zend\Stdlib\PriorityQueue, Zend\View\Exception;
 class AggregateResolver extends ZendAggregateResolver {
     public function resolve($name, Renderer $renderer = null) {
 		$name = str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace(DIRECTORY_SEPARATOR, ' ', trim($name))));
-        $this->lastLookupFailure      = false;
-        $this->lastSuccessfulResolver = null;
-
-        if (0 === count($this->queue)) {
-            $this->lastLookupFailure = static::FAILURE_NO_RESOLVERS;
-            return false;
-        }
-
-        foreach ($this->queue as $resolver) {
-            $resource = $resolver->resolve($name, $renderer);
-            if (!$resource) {
-                // No resource found; try next resolver
-                continue;
-            }
-
-            // Resource found; return it
-            $this->lastSuccessfulResolver = $resolver;
-            return $resource;
-        }
-
-        $this->lastLookupFailure = static::FAILURE_NOT_FOUND;
-        return false;
+		return parent::resolve($name, $renderer);
     }
 }
