d32b93a587b41a596b15e72d9650be3de3f09cc9
lam dreamd <lamdreamd@lam-dreamdteki-iMac.local>
2012-12-31 02:32:31 +0800

    move the autoloader setting to config

--- project/template/autoloader.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-		'Zend\Loader\StandardAutoloader' => array(
-				'autoregister_zf' => true
-		)
-);
---
+++ project/template/config/autoloader.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'Zend\Loader\StandardAutoloader' => array(
+		'autoregister_zf' => true,
+		'namespaces' => array(
+			'Dream' => 'Dream',
+			'Twig' => 'Twig',
+			'phpbrowscap' => 'PhpBrowscap'			  
+		),
+	)
+);
