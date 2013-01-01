9884855ff73091204eec77468fc6d6f093b9992c
lam dreamd <lamdreamd@lam-dreamdteki-iMac.local>
2013-01-02 02:40:01 +0800

    update zend renderer loader with script

--- library/Dream/Twig/Loader/ZendRendererLoader.php
+++ library/Dream/Twig/Loader/ZendRendererLoader.php
@@ -6,6 +6,12 @@ use Twig_LoaderInterface, Dream\Zend\View\Renderer\PhpRenderer ;
 
 class ZendRendererLoader implements Twig_LoaderInterface {
 	private $files = array(), $renderer = NULL, $templatePath = false;
+	const shareKeyword = 'SHARE';
+	const sharePath = 'ShareResources';
+	const currentPath = '.';
+	const prevPath = '..';
+	const rootPath = '~';
+	const folderPrefix = '/';
 	public function __construct(PhpRenderer $renderer) {
 		$this->renderer = $renderer;
 	}
@@ -16,8 +22,8 @@ class ZendRendererLoader implements Twig_LoaderInterface {
 	}
 	public function setTemplatePath($path = NULL) {
 		if (is_string($path) === true && empty($path) === false) {
-			if (substr($path, -1) !== '/') {
-				$this->templatePath = substr($path, 0, strrpos($path, '/') + 1);
+			if (substr($path, -1) !== self::folderPrefix) {
+				$this->templatePath = substr($path, 0, strrpos($path, self::folderPrefix) + 1);
 			} else {
 				$this->templatePath = $path;
 			}
@@ -30,32 +36,32 @@ class ZendRendererLoader implements Twig_LoaderInterface {
 		if ($name !== NULL && array_key_exists($name, $this->files) === true) {
 			return $this->files[$name];	
 		}
-		if (strpos($name, '.') === false) {
+		if (strpos($name, self::currentPath) === false) {
 			$path = $name;
 		} else {
-			$path = substr($name, 0, strrpos($name, '.'));
+			$path = substr($name, 0, strrpos($name, self::currentPath));
 		}
 		return $this->renderer->extend($this->fixPath($this->templatePath.$path));
     }
 	private function fixPath($path) {
-		$paths = array_filter(explode('/', $path));
+		$paths = array_filter(explode(self::folderPrefix, $path));
 		for ($i = 0; $i < max(array_keys($paths)); $i++) {
-			if (isset($paths[$i]) === true && $paths[$i] === 'SHARE') {
-				$paths[$i] = 'ShareFolder';
+			if (isset($paths[$i]) === true && $paths[$i] === self::shareKeyword) {
+				$paths[$i] = self::sharePath;
 				for ($j = $i - 1; $j >= 0; $j--) {
 					if (isset($paths[$j]) === true) {
 						unset($paths[$j]);
 					}
 				}
-			} else if (isset($paths[$i]) === true && $paths[$i] === '~') {
+			} else if (isset($paths[$i]) === true && $paths[$i] === self::rootPath) {
 				for ($j = $i; $j >= 0; $j--) {
 					if (isset($paths[$j]) === true) {
 						unset($paths[$j]);
 					}
 				}
-			} else if (isset($paths[$i]) === true && $paths[$i] === '.') {
+			} else if (isset($paths[$i]) === true && $paths[$i] === self::currentPath) {
 				unset($paths[$i]);
-			} else if (isset($paths[$i]) === true && $paths[$i] === '..') {
+			} else if (isset($paths[$i]) === true && $paths[$i] === self::prevPath) {
 				unset($paths[$i]);
 				for ($j = $i - 1; $j >= 0; $j--) {
 					if (isset($paths[$j]) === true) {
@@ -65,16 +71,18 @@ class ZendRendererLoader implements Twig_LoaderInterface {
 				}
 			}
 		}
-		return implode('/', array_map('ucfirst', $paths));
+		return implode(self::folderPrefix, array_map(function($value) {
+			return ucfirst($value);
+		}, $paths));
 	}
     public function getCacheKey($name = NULL) {
 		if ($name !== NULL && array_key_exists($name, $this->files) === true) {
 			return $name;	
 		}
-		if (strpos($name, '.') === false) {
+		if (strpos($name, self::currentPath) === false) {
 			$path = $name;
 		} else {
-			$path = substr($name, 0, strpos($name, '.'));
+			$path = substr($name, 0, strpos($name, self::currentPath));
 		}
         return $this->templatePath.$path;
     }
