8312dbb75cb18bafd9ceca918665846ceb897e10
lam dreamd <lamdreamd@lam-dreamdteki-iMac.local>
2013-01-04 00:47:33 +0800

    update autoloader

--- project/template/config/autoloader.php
+++ project/template/config/autoloader.php
@@ -3,9 +3,10 @@ return array(
 	'Zend\Loader\StandardAutoloader' => array(
 		'autoregister_zf' => true,
 		'namespaces' => array(
-			'Dream' => 'library/Dream',
-			'Twig' => 'library/Twig',
-			'phpbrowscap' => 'library/PhpBrowscap'			  
+			'Dream' 			=> 'library/Dream',
+			'Twig'				=> 'library/Twig',
+			'phpbrowscap'	=> 'library/PhpBrowscap',
+			'Everzet'			=> 'library/Everzet',
 		),
 	)
 );
