85c1c8e008acbd05f3e597812ba6ad003cfe033f
lam dreamd <lamdreamd@lam-dreamdteki-iMac.local>
2013-01-06 17:39:21 +0800

    add back l18n change, change the view helper l18n textdomain and locale exchange Position

--- library/Dream/Zend/Mvc/Service/ViewHelperManagerFactory.php
+++ library/Dream/Zend/Mvc/Service/ViewHelperManagerFactory.php
@@ -7,7 +7,7 @@ use Zend\Mvc\Service\ViewHelperManagerFactory as ZendViewHelperManagerFactory;
 class ViewHelperManagerFactory extends ZendViewHelperManagerFactory {
     protected $defaultHelperMapClasses = array(
         'Zend\Form\View\HelperConfig',
-        'Zend\I18n\View\HelperConfig',//will change
+        'Dream\Zend\I18n\View\HelperConfig',
         'Zend\Navigation\View\HelperConfig'
     );
 }
