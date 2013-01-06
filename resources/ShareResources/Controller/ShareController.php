9d3a0eaa442b982b1ca1ce247bb48a912387149e
lam dreamd <lamdreamd@lam-dreamdteki-iMac.local>
2013-01-06 17:25:55 +0800

    add the magic get set in controller

--- resources/ShareResources/Controller/ShareController.php
+++ resources/ShareResources/Controller/ShareController.php
@@ -5,6 +5,7 @@ use Dream\Zend\Mvc\Controller\AbstractActionController;
 use ShareResources\Controller\Exception\ControllerException;
 
 class ShareController extends AbstractActionController {
+	private $storage = array();
 	public function throwException($message = NULL) {
 		$message = is_string($message) === false ? 'Error' : $message;	
 		$trace = debug_backtrace();
@@ -14,5 +15,24 @@ class ShareController extends AbstractActionController {
 		$controllerException->setLine($caller->line);
 		
 		throw $controllerException;		
+	}
+	public function __get($name = NULL) {
+		if (is_string($name) === true && isset($this->storage[$name]) === true) {
+			return $this->storage[$name];
+		}
+		return NULL;
+	}
+	public function __set($name = NULL, $value = NULL) {
+		if (is_string($name) === true) {
+			$this->storage[$name] = $value;	
+		}
+	}
+	public function __isset($name = NULL) {
+		return isset($this->storage[$name]);
+	}
+	public function __unset($name) {
+		if (isset($this->storage[$name]) === true) {
+			unset($this->storage[$name]);	
+		}
 	}
 }
