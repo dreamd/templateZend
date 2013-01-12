1f4ef12ade51a957ae2ac80401a2b278b050d7f4
lam dreamd <lamdreamd@lam-dreamdteki-iMac.local>
2013-01-12 17:28:31 +0800

    add 'local version' about load resources / config now you can edit something to let the program load yours and no need to edit any about sources

--- resources/template/public/index.php
+++ resources/template/public/index.php
@@ -1,10 +1,38 @@
 <?php
 defined('__PROJECT__') === true || define('__PROJECT__', basename(dirname(__DIR__)));
 chdir(dirname(dirname(dirname(__DIR__))));
-require 'initAutoloader.php';
 
-$applicationConfig = require 'resources/'.__PROJECT__.'/config/application.php';
-$databaseConfig = require 'resources/'.__PROJECT__.'/config/database.php';
+$autoLoaderPath = 'initAutoloader.php';
+if (file_exists($autoLoaderPath) === false) {
+	throw new Exception('Have not auto loader.');
+}
+require $autoLoaderPath;
+
+$paths = (object)array(
+	'resources'					=> 'resources/',
+	'local'							=> 'Local/',
+);
+
+$loadConfigs = array(
+	'applicationConfig'		=> __PROJECT__.'/config/application.php',
+	'databaseConfig'		=> __PROJECT__.'/config/database.php',
+);
+
+$applicationConfig = array();
+$databaseConfig = array();
+
+foreach ($loadConfigs as $name => $value) {
+	if (file_exists($paths->resources.$paths->local.$value) === true) {
+		$$name = require $paths->resources.$paths->local.$value;
+		continue;
+	}
+	
+	$$name = require $paths->resources.$value;
+}
+
+if (empty($applicationConfig) === true) {
+	throw new Exception('Have not project setting.');
+}
 
 if (isset($applicationConfig['databases']) === true && isset($databaseConfig['databases']) === true) {
 	foreach ($applicationConfig['databases'] as $name => $value) {
